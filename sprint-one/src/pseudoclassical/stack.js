13757e71d3114294005a434de649c4d48213db4b
kathy-ems <kathyemssf@gmail.com>
2016-01-14 15:38:14 -0800

    Pseudoclassical instantiation for stack.js works.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,19 +3,38 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 
   //decalre this.storage object
+  this.storage = {};
   //declare this.stacksize variable and assgn it to zero
+  this.stacksize = 0;
 
 };
 
 //declare Stack.prototype.size function
+Stack.prototype.size = function () {
+  if(this.stacksize < 0) {
+    return 0;
+  }
   //return this.stacksize
+  return this.stacksize;
+};
 
 //declare Stack.protoype.push function with a vlue argument
-  //increment this.stacksize by one
+Stack.prototype.push = function (value) {
   //assign this.storage[this.stacksize] to the value
+  this.storage[this.stacksize] = value;
+  //increment this.stacksize by one
+  this.stacksize++;
+};
 
 //declare Stack.prototype.pop function
+Stack.prototype.pop = function () {
   //if this.stacksize is less than zero
+  if(this.stacksize < 0){
     //return
-  //decrement this.stacksize by one
+    return;
+  }
+  // decrement this.stacksize by one
+  this.stacksize--;
   // return this.storage[this.stacksize]
+  return this.storage[this.stacksize];
+};
