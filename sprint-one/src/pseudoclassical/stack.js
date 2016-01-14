969d1cd7abeeada9d5501292a987d0e454442127
kathy-ems <kathyemssf@gmail.com>
2016-01-14 13:51:44 -0800

    Updated prototype instantiation.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //decalre this.storage object
+  //declare this.stacksize variable and assgn it to zero
+
 };
 
+//declare Stack.prototype.size function
+  //return this.stacksize
 
+//declare Stack.protoype.push function with a vlue argument
+  //increment this.stacksize by one
+  //assign this.storage[this.stacksize] to the value
+
+//declare Stack.prototype.pop function
+  //if this.stacksize is less than zero
+    //return
+  //decrement this.stacksize by one
+  // return this.storage[this.stacksize]
