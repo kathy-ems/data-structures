a089122ea57026c1550b739ecaa19d25552a1ac4
kathy-ems <kathyemssf@gmail.com>
2016-01-14 18:21:18 -0800

    linkedList designates a new tail when new nodes are added

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,10 +3,30 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // create a variable to hold the previous node value
+  // this will be used to find the previous node and add the current value
+  var prevNodeValue = null;
+
   list.addToTail = function(value) {
+    list.tail = Node(value);
+    list.tail.value = value;
+
+    // set the node.next property of the previous node () to the current value
+    // list.tail.value[prevNodeValue] = value;
+
+    // if this is the first item (list.head = null)
+    if (list.head === null) {
+      // add the current value to list.head
+      list.head = value;
+    } 
+
+    // holds the current value in order to add it to the previous node as node.next value
+    prevNodeValue = value;
   };
 
   list.removeHead = function() {
+
+    return list.head;
   };
 
   list.contains = function(target) {
@@ -20,6 +40,7 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
+  
 
   return node;
 };
