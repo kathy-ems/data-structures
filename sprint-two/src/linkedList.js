36aeb5224f6fc9656cbfce7dd24ba23563618175
kathy-ems <kathyemssf@gmail.com>
2016-01-14 19:42:36 -0800

    wip

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,31 +1,30 @@
 var LinkedList = function() {
   var list = {};
   list.head = null;
-  list.tail = null;
+  list.tail = null; //4
 
   // create a variable to hold the previous node value
   // this will be used to find the previous node and add the current value
-  var prevNodeValue = null;
+  list.prevNode = null;
 
   list.addToTail = function(value) {
-    list.tail = Node(value);
-    list.tail.value = value;
 
-    // set the node.next property of the previous node () to the current value
-    // list.tail.value[prevNodeValue] = value;
+    if(list.tail === null ){
+      list.tail = Node(value);
+    } else {
+      list.tail.next = value;
+      list.tail = Node(value);
+    }
 
     // if this is the first item (list.head = null)
     if (list.head === null) {
-      // add the current value to list.head
+      // add the current value to list.head for quick lookups
       list.head = value;
     } 
-
-    // holds the current value in order to add it to the previous node as node.next value
-    prevNodeValue = value;
   };
 
   list.removeHead = function() {
-
+    // 
     return list.head;
   };
 
