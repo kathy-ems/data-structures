42a5c2b49c967256a6fdbbe66c4d18a899840a6c
kathy-ems <kathyemssf@gmail.com>
2016-01-15 11:11:09 -0800

    Adds to tree, adds children, returns t/f for whether children are in tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,9 +14,36 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // check if current node.value = target. 
+  // if(this.value === target) {
+  //   return true;
+  // }
+
+  var checkcontains = function(element) {
+    // base case: if el.length equals 0 return false
+    if(element.length === 0) {
+      return false;
+    }
+    if(element[0].value === target) {
+      return true;
+    } else {
+      // to find nested children
+      if(element.children !== undefined) {
+        var temp = element.children;
+        for(var i = 0;i<temp.length;i++) {
+         return checkcontains(temp[i]);
+        }
+      }
+      // returnn checkcontains with everything else in element besides first (use slice)
+      return checkcontains(element.slice(1));
+    }
+  };
+  // call checkcotnains on this.children
+  return checkcontains(this.children);
 };
 
 
