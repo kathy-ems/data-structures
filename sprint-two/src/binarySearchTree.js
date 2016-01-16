9e5cf940cbb1567f6ea1e4390c1e582ede6febe5
kathy-ems <kathyemssf@gmail.com>
2016-01-16 15:46:52 -0800

    binary search tree has functions for addChild, contains, and property of value

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,44 @@
 var BinarySearchTree = function(value) {
+  
+  var treeNode = Object.create(treeNodeMethods);
+
+  treeNode.value = value;
+  treeNode.right = null;
+  treeNode.left = null;
+
+  return treeNode;
+
 };
 
+var treeNodeMethods = {};
+
+// methods
+treeNodeMethods.insert = function(value) {
+  var oldValue = this.value;
+
+  if(value < oldValue) {
+    if(this.left === null) {
+      this.left = treeNode(value);
+    } else {
+      insert(this.left);
+    }
+  }
+  if(value > oldValue) {
+    if(this.right === null) {
+      this.right = treeNode(value);
+    } else {
+      insert(this.right);
+    } 
+  }
+};
+
+treeNodeMethods.contains = function() {
+
+};
+
+treeNodeMethods.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
